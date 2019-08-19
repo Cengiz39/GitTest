73989d251f4fea2a630eba2de49207775a5b692a
Cengiz Baygın <qrowlite02@icloud.com>
2019-08-19 21:31:51 +0300

    Testing

---
+++ GitTestProjects/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  GitTestProjects
+//
+//  Created by Cengiz Baygın on 19.08.2019.
+//  Copyright © 2019 Cengiz Baygın. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("GitTestProject 19.08.19 MacOs Swift")
+    }
+
+
+}
+
